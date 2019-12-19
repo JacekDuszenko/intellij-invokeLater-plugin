5fffa0a8e8ef26ae0a1d5e748248a2f41366aa27
Jacek Duszenko <jacek.duszenko@huuugegames.com>
2019-12-19 04:17:23 +0100

    Takeoff
    Changes made:
    - bootstraped gradle project with intellij-gradle-plugin and structure.
    - added hello world action

---
+++ src/main/java/HelloAction.java
@@ -0,0 +1,23 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import org.jetbrains.annotations.NotNull;
+
+public class HelloAction extends AnAction {
+
+    public HelloAction() {
+        super("yo");
+    }
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        Project project = e.getProject();
+        Messages.showMessageDialog(project, "Hello fellow!", "Asd", Messages.getQuestionIcon());
+    }
+
+    @Override
+    public boolean isDumbAware() {
+        return false;
+    }
+}
